af0aeb1ceda404be2f5360b884601e9722a83e01
rite2rio <marionoronha.s@gmail.com>
2022-07-09 15:57:10 +0530

    included the ml method

--- model.py
+++ model.py
@@ -3,4 +3,13 @@ print("ml related code here")
 def model1()
     print("adding code for ML Model 1")
 
-    
+
+def model2()
+    print("adding code for ML Model 1")
+
+
+def model3()
+    print("adding code for ML Model 1")
+
+
+
